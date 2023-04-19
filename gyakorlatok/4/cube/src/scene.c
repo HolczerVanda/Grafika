3218fbcc87d84687386106891677ff1d5450f3a4
HolczerVanda <vanda.holczer@gmail.com>
2023-04-19 18:29:06 +0200

    Made some changes to the colors and lighting.

--- gyakorlatok/4/cube/src/scene.c
+++ gyakorlatok/4/cube/src/scene.c
@@ -16,28 +16,28 @@ void init_scene(Scene *scene)
 
     glBindTexture(GL_TEXTURE_2D, scene->texture_id);
 
-    scene->material.ambient.red = 0.0;
+    scene->material.ambient.red = 1.0;
     scene->material.ambient.green = 0.0;
     scene->material.ambient.blue = 0.0;
 
-    scene->material.diffuse.red = 1.0;
+    scene->material.diffuse.red = 0.0;
     scene->material.diffuse.green = 1.0;
     scene->material.diffuse.blue = 0.0;
 
     scene->material.specular.red = 0.0;
     scene->material.specular.green = 0.0;
-    scene->material.specular.blue = 0.0;
+    scene->material.specular.blue = 1.0;
 
-    scene->rotation_speed = 0.0f;
+    scene->rotation_speed = 1.0f;
 
-    scene->material.shininess = 0.0;
+    scene->material.shininess = 100.0;
 }
 
 void set_lighting()
 {
     float ambient_light[] = { 0.0f, 0.0f, 0.0f, 1.0f };
-    float diffuse_light[] = { 1.0f, 1.0f, 1.0, 1.0f };
-    float specular_light[] = { 0.0f, 0.0f, 0.0f, 1.0f };
+    float diffuse_light[] = { 0.0f, 0.0f, 1.0, 0.0f };
+    float specular_light[] = { 0.0f, 1.0f, 0.0f, 0.0f };
     float position[] = { 0.0f, 0.0f, 10.0f, 1.0f };
 
     glLightfv(GL_LIGHT0, GL_AMBIENT, ambient_light);
